f1a4504527b509a116b5c8bfcb98a192e39f7f50
Jonathan Johnson <me@jondavidjohn.com>
2024-02-06 16:55:31 -0600

    remove key

--- main.tf
+++ main.tf
@@ -58,10 +58,6 @@ resource "azurerm_linux_virtual_machine" "example" {
     azurerm_network_interface.example.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
